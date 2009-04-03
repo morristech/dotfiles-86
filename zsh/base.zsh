29cb826ebfb60006b4ac04ac8e3ebac5ba842d10
Tom Ward <tom@popdog.net>
2009-04-03 16:31:42 +0100

    Added simple title bar script (robbed from bryanl), and an aa alias to git

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -12,6 +12,7 @@ esac
 source ~/.houseshare/zsh/aliases.zsh
 source ~/.houseshare/zsh/completion.zsh
 source ~/.houseshare/zsh/prompt.zsh
+source ~/.houseshare/zsh/title.zsh
 source ~/.houseshare/zsh/misc.zsh
 
 # Load vendor specific scripts
