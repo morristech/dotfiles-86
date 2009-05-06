ad5b6f16ed14534f402078b9d13d02e6c6cb2cf7
Tom Ward <tom@popdog.net>
2009-05-06 13:47:42 +0100

    Changed aliases into commands!  Just an experiment really...

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,28 @@
 # command aliases
 
-alias ss='ruby script/server -u'
-alias sc='ruby script/console'
-alias db='ruby script/dbconsole'
+ss () {
+  if [[ -a script/console ]] 
+  then
+    ruby script/server -u $*
+  else
+    echo "script/server could not found. Maybe this isn't a rails project?"
+  fi
+}
+
+sc () {
+  if [[ -a script/console ]] 
+  then
+    ruby script/console $*
+  else
+    echo "script/console could not found. Maybe this isn't a rails project?"
+  fi
+}
+
+db () {
+  if [[ -a script/dbconsole ]] 
+  then
+    ruby script/dbconsole $*
+  else
+    echo "script/dbconsole could not found. Maybe this isn't a rails project?"
+  fi
+}
