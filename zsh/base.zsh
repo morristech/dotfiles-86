ce68abf724c142f8632c701b366dbec769a6f0d8
Tom Ward <tom@popdog.net>
2009-08-21 19:56:11 +0100

    Adding defunkt's ruby_or_irb trick.  Or is is Evan Phoenix's?

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -9,6 +9,8 @@ source ~/.dotfiles/zsh/history.zsh
 fpath=(~/.dotfiles/zsh/functions $fpath)
 autoload -U ~/.dotfiles/zsh/functions/*(:t)
 
+alias ruby="ruby_or_irb"
+
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
 
