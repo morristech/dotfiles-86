dc8118cd458cd0460a263f2b811cf5feae1d535f
Tom Ward <tom@popdog.net>
2009-09-22 08:22:29 +0100

    Added rail alias, and postgres path to PATH

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -1,5 +1,7 @@
 system_name=`uname -a`
 
+source ~/.dotfiles/zsh/env.zsh
+
 fpath=(~/.dotfiles/zsh/functions $fpath)
 
 source ~/.dotfiles/zsh/completion.zsh
@@ -11,6 +13,7 @@ source ~/.dotfiles/zsh/history.zsh
 autoload -U ~/.dotfiles/zsh/functions/*(:t)
 
 alias ruby="ruby_or_irb"
+alias rails="rails -m ~/.dotfiles/resources/rails-template.rb"
 
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,4 +1,4 @@
 RIPDIR=~/.rip
 RUBYLIB="$RUBYLIB:$RIPDIR/active/lib"
-PATH="$PATH:$RIPDIR/active/bin"
+PATH="$PATH:$RIPDIR/active/bin:/opt/local/lib/postgresql83/bin"
 export RIPDIR RUBYLIB PATH
