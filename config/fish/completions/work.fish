23ba39d074359776f2f12f33483fe2f77a9d099c
Tom Ward <tom@popdog.net>
2018-09-13 12:16:30 +0100

    Use functions and completions from homebrew installed work

--- config/fish/completions/work.fish
+++
@@ -1,2 +0,0 @@
-complete -f -c 'work' -n '__fish_use_subcommand' -a 'browse cd clone edit on open path resolve'
-complete -f -c 'work' -n '__fish_seen_subcommand_from browse cd clone edit on open path resolve' -a '(work list)'
