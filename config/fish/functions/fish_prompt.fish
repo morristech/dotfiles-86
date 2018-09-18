a2550ed5d0ef3e723615d428e74e27075d2fec29
Tom Ward <tom@popdog.net>
2018-09-18 21:52:39 +0100

    Show duration of long running commands after completion

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,4 +1,18 @@
+function __fish_prompt_duration
+  set --local duration (echo "$CMD_DURATION 1000" | awk '{printf "%.2fs", $1 / $2}')
+  set --local last (string split " " (echo $history[1]))
+
+  set_color yellow
+  echo -n "  $last[1] took ~"
+  echo $duration
+  set_color normal
+end
+
 function fish_prompt
+  if test $CMD_DURATION -gt 1000
+    echo (__fish_prompt_duration)
+  end
+
   set_color blue
   echo -n '❯ '
   set_color normal
