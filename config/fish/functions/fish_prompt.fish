0e50bb16b18fd02839dbccc431b9a1a0a53a8ec0
Tom Ward <tom@popdog.net>
2018-09-24 17:40:51 +0100

    Different colours

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -2,7 +2,7 @@ function __fish_prompt_duration
   set --local duration (echo "$CMD_DURATION 1000" | awk '{printf "%.2fs", $1 / $2}')
   set --local last (string split " " (echo $history[1]))
 
-  set_color yellow
+  set_color 777
   echo -n "  $last[1] took ~"
   echo $duration
   set_color normal
@@ -13,7 +13,8 @@ function fish_prompt
     echo (__fish_prompt_duration)
   end
 
-  set_color blue
+  set_color c1e3fe
+
   echo -n '❯ '
   set_color normal
 end
