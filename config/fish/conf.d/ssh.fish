aadd43945c89e0a3e6cc23c45cc4ccd31c93b621
Tom Ward <tom@popdog.net>
2018-09-27 12:20:11 +0100

    Generate an ssh-key and add it to authorized_keys

--- config/fish/conf.d/ssh.fish
+++ config/fish/conf.d/ssh.fish
@@ -1,1 +1,20 @@
+dotfiles-link $HOME/.dotfiles/ssh/authorized_keys $HOME/.ssh/authorized_keys
+
+if dotfiles-is-installing
+  set --local hostName (string replace ".local" "" (hostname))
+  set --local keyName "tom@$hostName.ed25519"
+
+  if not test -e ~/.ssh/$keyName
+    ssh-keygen -t ed25519 -a 100 -f ~/.ssh/$keyName -N '' -C tom@$hostName
+  end
+
+  if not grep (cat ~/.ssh/$keyName.pub) ~/.ssh/authorized_keys
+    cat ~/.ssh/$keyName.pub >> ~/.ssh/authorized_keys
+  end
+
+  if not ssh-add -L | grep (cat ~/.ssh/$keyName.pub)
+    ssh-add ~/.ssh/$keyName
+  end
+end
+
 abbr --add ssh '/usr/bin/ssh -A'
