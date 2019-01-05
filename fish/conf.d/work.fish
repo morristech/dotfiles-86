44ec75228488d4d541da074f28630e2cb1415aed
Tom Ward <tom@popdog.net>
2019-01-05 12:41:59 +0000

    Use vendor work function, not our own

--- fish/conf.d/work.fish
+++
@@ -1,9 +0,0 @@
-set WORK_EDITOR "code --new-window"
-
-abbr --add wo work on
-abbr --add bc3 work cd basecamp/bc3
-abbr --add hr work cd highrisehq/server
-abbr --add dot work cd tomafro/dotfiles
-abbr --add qb work cd basecamp/queenbee
-
-work update --if-needed
